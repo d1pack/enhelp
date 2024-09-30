78ea26bd4ae44c8578eb1f96c54fca61d1cfa0cd
luxar <ailab73@mail;.ru>
2024-09-30 12:45:20 +0400

    docker

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -77,12 +77,15 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "django",
+        "USER": "luxar",
+        "PASSWORD": "sYst3m0ps",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
